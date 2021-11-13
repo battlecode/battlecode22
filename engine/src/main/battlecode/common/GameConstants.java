0aa238e49097154da7cd0a3aee7779386032fb2e
Nicole Wong <wongn@mit.edu>
2021-11-13 09:48:37 -0500

    Added new RobotType variables, functions, GameConstants.

--- engine/src/main/battlecode/common/GameConstants.java
+++ engine/src/main/battlecode/common/GameConstants.java
@@ -43,41 +43,54 @@ public class GameConstants {
     // *********************************
     // ****** COOLDOWNS ****************
     // *********************************
- 
+
+    // NEED TO CHANGE: what is the actual cooldown value for the following values:
+
+    public static final int BUILDING_UPGRADE_COOLDOWN = 500;
+    public static final float FRESH_ROBOT_ARCHON_COOLDOWN = 10;
+    // turret to portable, or portable to turret
+    public static final int BUILDING_CONVERSION_COOLDOWN = 10000;
+
+
+    // *********************************
+    // ****** LEVEL MULTIPLIERS ********
+    // *********************************
+
+
+    public static final float PLACEHOLDER_LEVEL_MULTIPLIER = 1.2;
+
+    public static final float DPS_1_TO_2 = PLACEHOLDER_LEVEL_MULTIPLIER;
+    public static final float HP_1_TO_2 = PLACEHOLDER_LEVEL_MULTIPLIER;
+
+    public static final float DPS_2_TO_3 = PLACEHOLDER_LEVEL_MULTIPLIER;
+    public static final float HP_2_TO_3 = PLACEHOLDER_LEVEL_MULTIPLIER;
+
+    public static final float DPS_UNIT_TO_TURRET = PLACEHOLDER_LEVEL_MULTIPLIER;
+    public static final float HP_UNIT_TO_TURRET = PLACEHOLDER_LEVEL_MULTIPLIER;
+
+    // NEED TO CHANGE:
+    // turret vision = unit vision + ???
+    // building portable mode move cooldown = ???
+
+    public static final float RICOCHET_UPGRADE_MULTIPLIER = 1.5;
+    public static final float BUILDING_UPGRADE_COST_MULTIPLER = 0.5;
+
 
     // *********************************
     // ****** GAME MECHANICS ***********
     // *********************************
 
-    /** The amount of conviction taxed when a Politician empowers. */
-    public static final int EMPOWER_TAX = 10;
+    public static final float PROTOTYPE_HP_PERCENTAGE = 0.1;
+    public static final float AUTOMATIC_ARCHON_HEAL_AMOUNT = 10;
+    public static final float AUTOMATIC_BUILDER_HEAL_AMOUNT = 10;
 
-    /** The buff factor from exposing Slanderers. */
-    public static final double EXPOSE_BUFF_FACTOR = 0.001;
+    public static final float PER_ALCHEMIST_DECREASE_RATE = 0.1;
+
+    public static final int LEAD_TO_GOLD_RATE = 20;
+    public static final int INITIAL_LEAD = 200;
     
-    /** The number of rounds a buff is applied. */
-    public static final int EXPOSE_BUFF_NUM_ROUNDS = 50;
 
-    /** The number of rounds Slanderers generate influence. */
-    public static final int EMBEZZLE_NUM_ROUNDS = 50;
-
-    /** The scale factor in the Slanderer embezzle influence formula. */
-    public static final float EMBEZZLE_SCALE_FACTOR = 0.03f;
-
-    /** The exponential decay factor in the Slanderer embezzle influence formula. */
-    public static final float EMBEZZLE_DECAY_FACTOR = 0.001f;
-
-    /** The number of rounds before Slanderers turns into Politicians. */
-    public static final int CAMOUFLAGE_NUM_ROUNDS = 300;
-
-    /** The initial amount of influence for each player-owned Enlightenment Center. */
-    public static final int INITIAL_ENLIGHTENMENT_CENTER_INFLUENCE = 150;
-
-    /** The passive influence ratio for Enlightenment Centers. To multiply by sqrt(roundNum). */
-    public static final float PASSIVE_INFLUENCE_RATIO_ENLIGHTENMENT_CENTER = 0.2f;
-
-    /** Maximum allowable robot influence. */
-    public static final int ROBOT_INFLUENCE_LIMIT = 100000000;
+    // Older constants below, maintaining them for now.
 
     /** The minimum allowable flag value. */
     public static final int MIN_FLAG_VALUE = 0;
