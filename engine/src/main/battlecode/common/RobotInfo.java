893a9246d5ebbbc08b28cad5048b0d9c80fc46f5
Elizabeth Zou <ezou@mit.edu>
2021-12-04 12:32:44 -0500

    robotinfo

--- engine/src/main/battlecode/common/RobotInfo.java
+++ engine/src/main/battlecode/common/RobotInfo.java
@@ -23,6 +23,11 @@ public class RobotInfo {
     public final RobotType type;
 
     /**
+     * The level of the robot.
+     */
+    public final int level;
+
+    /**
      * The health of the robot.
      */
     public final int health;
@@ -32,11 +37,12 @@ public class RobotInfo {
      */
     public final MapLocation location;
 
-    public RobotInfo(int ID, Team team, RobotType type, int health, MapLocation location) {
+    public RobotInfo(int ID, Team team, RobotType type, int level, int health, MapLocation location) {
         super();
         this.ID = ID;
         this.team = team;
         this.type = type;
+        this.level = level;
         this.health = health;
         this.location = location;
     }
@@ -69,6 +75,15 @@ public class RobotInfo {
     }
 
     /**
+     * Returns the level of this robot.
+     *
+     * @return the level of this robot.
+     */
+    public int getLevel() {
+        return level;
+    }
+
+    /**
      * Returns the health of this robot.
      *
      * @return the health of this robot
@@ -96,6 +111,7 @@ public class RobotInfo {
         if (ID != robotInfo.ID) return false;
         if (team != robotInfo.team) return false;
         if (type != robotInfo.type) return false;
+        if (level != robotInfo.level) return false;
         if (health != robotInfo.health) return false;
         return location.equals(robotInfo.location);
     }
@@ -106,6 +122,7 @@ public class RobotInfo {
         result = ID;
         result = 31 * result + team.hashCode();
         result = 31 * result + type.ordinal();
+        result = 31 * result + level;
         result = 31 * result + health;
         result = 31 * result + location.hashCode();
         return result;
@@ -117,6 +134,7 @@ public class RobotInfo {
                 "ID=" + ID +
                 ", team=" + team +
                 ", type=" + type +
+                ", level=" + level +
                 ", health=" + health +
                 ", location=" + location +
                 '}';
