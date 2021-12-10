87a9a6b16b8d2f34039722aa7c72ea996ad5062f
Elizabeth Zou <ezou@mit.edu>
2021-12-10 05:45:35 -0500

    lol

--- engine/src/main/battlecode/common/GameConstants.java
+++ engine/src/main/battlecode/common/GameConstants.java
@@ -9,7 +9,6 @@ public class GameConstants {
     /**
      * The current spec version the server compiles with.
      */
-     
     public static final String SPEC_VERSION = "1.0";
 
     // *********************************
@@ -61,48 +60,11 @@ public class GameConstants {
     /** A prototype building's starting health, as a multiplier of max health. */
     public static final float PROTOTYPE_STARTING_HEALTH_MULTIPLIER = 0.1f;
 
-    // TODO: what is the actual cooldown value for the following values:
-
-    public static final int BUILDING_UPGRADE_COOLDOWN = 500;
-    public static final float FRESH_ROBOT_ARCHON_COOLDOWN = 10f;
-    // turret to portable, or portable to turret
-    public static final int BUILDING_CONVERSION_COOLDOWN = 10;
-    public static final float BUILDING_UPGRADE_COST_MULTIPLER = 0.5f;
-
-    // *********************************
-    // ****** LEVEL MULTIPLIERS ********
-    // *********************************
-
-    public static final float PLACEHOLDER_LEVEL_MULTIPLIER = 1.2f;
-
-    public static final float DPS_1_TO_2 = PLACEHOLDER_LEVEL_MULTIPLIER;
-    public static final float HP_1_TO_2 = PLACEHOLDER_LEVEL_MULTIPLIER;
-
-    public static final float DPS_2_TO_3 = PLACEHOLDER_LEVEL_MULTIPLIER;
-    public static final float HP_2_TO_3 = PLACEHOLDER_LEVEL_MULTIPLIER;
-
-    public static final float DPS_UNIT_TO_TURRET = PLACEHOLDER_LEVEL_MULTIPLIER;
-    public static final float HP_UNIT_TO_TURRET = PLACEHOLDER_LEVEL_MULTIPLIER;
-
-    // TODO
-    // turret vision = unit vision + ???
-    // building portable mode move cooldown = ???
-
-    // *********************************
-    // ****** GAME MECHANICS ***********
-    // *********************************
-
+    /** The amount of health a prototype building has as a multiplier. */
     public static final float PROTOTYPE_HP_PERCENTAGE = 0.1f;
-    public static final float AUTOMATIC_ARCHON_HEAL_AMOUNT = 10f;
-    public static final float AUTOMATIC_BUILDER_HEAL_AMOUNT = 10f;
 
-    public static final float PER_ALCHEMIST_DECREASE_RATE = 0.1f;
-
-    public static final int LEAD_TO_GOLD_RATE = 20;
-    public static final int INITIAL_LEAD = 200;
-    public static final float PROTOTYPE_HP_PERCENTAGE = 0.9f;
-
-    // Older constants below, maintaining them for now.
+    /** The multiplier for reclaiming a building's cost. */
+    public static final float RECLAIM_COST_MULTIPLIER = 0.2f;
 
     /** The maximum level a building can be. */
     public static final int MAX_LEVEL = 3;
