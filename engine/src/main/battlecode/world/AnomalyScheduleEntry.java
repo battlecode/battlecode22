8646f753ae350c804cce02b88e3c4764adfcea40
Nicole Wong <wongn@mit.edu>
2021-12-10 12:42:56 -0500

    Minor changes to anomaly

--- engine/src/main/battlecode/world/AnomalyScheduleEntry.java
+++ engine/src/main/battlecode/world/AnomalyScheduleEntry.java
@@ -2,14 +2,12 @@ package battlecode.world;
 
 public class AnomalyScheduleEntry {
 
-    private int roundNumber;
-    public AnomalyInfo anomalyType;
+    public final int roundNumber;
+    public final AnomalyInfo anomalyType;
 
     public AnomalyScheduleEntry(int round, int anomaly){
         this.roundNumber = round;
         this.anomalyType = anomaly;
     }
-
-    
 
 }
