e443dbbd8f367457bad5b1df9e266b52e121140d
Elizabeth Zou <ezou@mit.edu>
2021-12-21 17:02:37 -0600

    delete extra lines

--- engine/src/main/battlecode/common/AnomalyType.java
+++ engine/src/main/battlecode/common/AnomalyType.java
@@ -4,25 +4,21 @@ package battlecode.common;
  * Holds the different anomalies in the game.
  */
 public enum AnomalyType {
-
     ABYSS       (true,    true,   0.1,    0.2),
     CHARGE      (true,    true,   0.05,   0.1),
     FURY        (true,    true,   0.05,   0.1),
     VORTEX      (true,    false,  0,      0),
     SINGULARITY (true,    false,  0,      0);
-    
 
     public final boolean isGlobalAnomaly;
     public final boolean isSageAnomaly;
     public final float globalPercentage;
     public final float sagePercentage;
 
-
     AnomalyType(boolean isGlobalAnomaly, boolean isSageAnomaly, float globalPercentage, float sagePercentage) {
         this.isGlobalAnomaly     = isGlobalAnomaly;
         this.isSageAnomaly       = isSageAnomaly;
         this.globalPercentage    = globalPercentage;
         this.sagePercentage      = sagePercentage;
-
     }
 }
