db757c3835a8600cd54a234695f3229ac0efb025
Elizabeth Zou <ezou@mit.edu>
2021-12-22 14:18:25 -0600

    TestMapBuilder

--- engine/src/main/battlecode/world/TestMapBuilder.java
+++ engine/src/main/battlecode/world/TestMapBuilder.java
@@ -8,58 +8,33 @@ import java.util.List;
  * Lets maps be built easily, for testing purposes.
  */
 public class TestMapBuilder {
-    private String name;
-    private MapLocation origin;
-    private int width;
-    private int height;
-    private int seed;
-    private int rounds;
-    private int[] rubbleArray;
+    private MapBuilder mapBuilder;
 
-    private List<RobotInfo> bodies;
-
-    public TestMapBuilder(String name, int oX, int oY, int width, int height, int seed, int rounds) {
-        this(name, new MapLocation(oX, oY), width, height, seed, rounds);
+    public TestMapBuilder(String name, int oX, int oY, int width, int height, int seed) {
+        this.mapBuilder = new MapBuilder(name, width, height, oX, oY, seed)
     }
 
-    public TestMapBuilder(String name, MapLocation origin, int width, int height, int seed, int rounds) {
-        this.name = name;
-        this.origin = origin;
-        this.width = width;
-        this.height = height;
-        this.seed = seed;
-        this.bodies = new ArrayList<>();
-    }
-
-    public TestMapBuilder addEnlightenmentCenter(int id, Team team, int influence, MapLocation loc) {
-        bodies.add(new RobotInfo(
-                id,
-                team,
-                RobotType.ENLIGHTENMENT_CENTER,
-                influence,
-                0,
-                loc
-        ));
-
+    public TestMapBuilder addArchon(int id, Team team, MapLocation loc) {
+        this.mapBuilder.addArchon(id, team, loc);
         return this;
     }
     
-    public TestMapBuilder setRubble() {
-        this.rubbleArray = new int[width * height];
-        for (int i = 0; i < width; i++) {
-            for (int j = 0; j < height; j++) {
-                this.rubbleArray[i + j * width] = (i * j + i + j) / (i * j + 1);
-            }
-        }
+    public TestMapBuilder setRubble(int x, int y, int value) {
+        this.mapBuilder.setRubble(x, y, value);
         return this;
     }
 
-    public TestMapBuilder addBody(RobotInfo info) {
-        bodies.add(info);
+    public TestMapBuilder setLead(int x, int y, int value) {
+        this.mapBuilder.setLead(x, y, value);
+        return this;
+    }
+
+    public TestMapBuilder addAnomalyScheduleEntry(int round, AnomalyType anomaly) {
+        this.mapBuilder.addAnomalyScheduleEntry(round, anomaly);
         return this;
     }
 
     public LiveMap build() {
-        return new LiveMap(width, height, origin, seed, GameConstants.GAME_MAX_NUMBER_OF_ROUNDS, name, bodies.toArray(new RobotInfo[bodies.size()]), rubbleArray);
+        return this.mapBuilder.build();
     }
 }
