c7846258347e36c98a3370375905b0d6a2d25eee
Gaurav Arya <gauravarya272@gmail.com>
2022-01-02 17:55:08 -0500

    another tweak

--- client/visualizer/src/main/controls.ts
+++ client/visualizer/src/main/controls.ts
@@ -444,7 +444,8 @@ export default class Controls {
     let infoString = `<span class="info-name">ID:</span> <span class="info-num">${id}</span> | `;
     infoString += `<span class="info-name">Location:</span> <span class="info-num">(${x}, ${y})</span> | `;
     infoString += `<span class="info-name">Level:</span> <span class="info-num">${level}</span> | `; 
-    if (portable !== undefined && prototype !== undefined) infoString += `<span class="info-name">Mode:</span> <span class="info-num">${portable ? 'Port' : prototype ? 'Prot' : 'Turr'}</span><br>`;
+    if (portable !== undefined && prototype !== undefined) infoString += `<span class="info-name">Mode:</span> <span class="info-num">${portable ? 'Port' : prototype ? 'Prot' : 'Turr'}</span>`;
+    infoString += `<br>`;
     infoString += `<span class="info-name">HP:</span> <span class="info-num">${hp}</span> / <span class="info-num">${max_hp}</span> | `;      
     infoString += `<span class="info-name">DP:</span> <span class="info-num">${dp}</span> | `;    
     infoString += `<span class="info-name">Bytecodes Used:</span> <span class="info-num">${bytecodes}</span>`;
