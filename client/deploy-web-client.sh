fedf076d77af48891b7dd1121a3f0f7e5b2cb90c
n8kim1 <n8kim1@yahoo.com>
2022-01-03 01:14:29 -0800

    Add the web-based-client deploy script

---
+++ client/deploy-web-client.sh
@@ -0,0 +1,43 @@
+# TODO enforce state of repo up-to-date
+# TODO year as arg
+# TODO prereqs: npm, tsc, java?, flatc
+
+# TODO check rel_java.md sidebyside to see if any missing steps
+
+BUCKET_NAME="bc-game-storage"
+# TODO make gs://$BUCKET_NAME/clients/2022/ a var, and use it
+
+echo "IMPORTANT: Make sure to update gameVersion in client/visualizer/src/config."
+read -p "When done, press enter to proceed..."
+
+cd ../schema
+npm install
+npm run build
+# TODO the following replacement could be done automatically i think
+echo "IMPORTANT: "
+echo "NOW, change line 3 of schema/ts/battlecode_generated.ts to \`import { flatbuffers } from \"flatbuffers\"\`."
+echo "DON'T FORGET TO SAVE!"
+read -p "When done, press enter to proceed..."
+cd ../client
+
+cd playback
+npm install
+npm run build
+cd ../visualizer
+npm install
+npm run prod
+cd ..
+
+gsutil -m rm gs://$BUCKET_NAME/clients/2022/**
+gsutil -m cp -r visualizer/out gs://$BUCKET_NAME/clients/2022/
+gsutil -m cp visualizer/visualizer.html gs://$BUCKET_NAME/clients/2022/
+gsutil -m setmeta -h "Cache-Control:no-cache" -r gs://$BUCKET_NAME/clients/2022/**
+
+echo 'Do you wish to deploy the client to the public? Type public and press enter.'
+echo 'IMPORTANT: NEVER DO THIS BEFORE THE GAME IS RELEASED'
+read -p 'input: ' ispublic
+if [ "$ispublic" == 'public' ]
+then
+    gsutil -m acl ch -u AllUsers:R -r gs://$BUCKET_NAME/clients/2022/**
+fi
+
