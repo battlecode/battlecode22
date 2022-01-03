c8e0e6dedf59e4bcc2d6ddc372aaec4ba829096c
n8kim1 <n8kim1@yahoo.com>
2022-01-03 01:52:02 -0800

    fix directory locations, due to move

--- deploy/deploy-specs.sh
+++ deploy/deploy-specs.sh
@@ -1,6 +1,5 @@
-# TODO this script is not in the specs folder, on purpose --
+# Note: this script is not in the specs folder, on purpose --
 # I don't want this script to get deployed too.
-# A better idea is to move all deploy scripts to a separate folder and then redo cd's.
 
 BUCKET_NAME="bc-game-storage"
 
@@ -12,9 +11,9 @@ read -p 'input: ' ispublic
 # Clean
 gsutil -m rm gs://$BUCKET_NAME/specs/2022/**
 # Upload
-cd specs
-gsutil -m cp -r * gs://$BUCKET_NAME/specs/2022/
-cd ..
+cd ../specs
+gsutil -m cp -r ** gs://$BUCKET_NAME/specs/2022/
+cd ../deploy
 # Enforce cache policy
 gsutil -m setmeta -h "Cache-Control:no-cache" -r gs://$BUCKET_NAME/specs/2022/**
 # Make public?
