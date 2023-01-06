6ed05b679c0822e9bbe332812ff5655812dd023e
Jerry Mao <j-mao@users.noreply.github.com>
2023-01-06 16:24:32 +1100

    Reset versions and update build scripts

--- client/visualizer/src/config.ts
+++ client/visualizer/src/config.ts
@@ -146,7 +146,7 @@ export enum Mode {
  */
 export function defaults(supplied?: any): Config {
   let conf: Config = {
-    gameVersion: "2022.2.2.4", //TODO: Change this on each release!
+    gameVersion: "2.2.1", //TODO: Change this on each release!
     fullscreen: false,
     width: 600,
     height: 600,
